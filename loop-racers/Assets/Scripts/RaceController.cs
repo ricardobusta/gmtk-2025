66d690ec98793bbdce124ca23bd84d601e2f7104
Ricardo Bustamante <ricardo@busta.dev>
2025-08-02 14:50:26 -0300

    Add splines

---
+++ loop-racers/Assets/Scripts/RaceController.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+using UnityEngine.Splines;
+
+public class RaceController : MonoBehaviour
+{
+    [SerializeField] private GameObject car;
+    [SerializeField] private SplineContainer spline;
+    [SerializeField] private float acceleration = 5.0f;
+    [SerializeField] private float drag = 10.0f;
+    [SerializeField] private float maxSpeed = 100;
+
+
+    private float _speed;
+    private float _meterToSplineUnit;
+    private float _parametricPosition;
+
+    private void Start()
+    {
+        _meterToSplineUnit = 1.0f / spline.CalculateLength();
+        _parametricPosition = 0;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _speed += acceleration * Time.deltaTime;
+        }
+        else
+        {
+            _speed -= drag * Time.deltaTime;
+        }
+
+        _speed = Mathf.Clamp(_speed, 0, maxSpeed);
+
+        _parametricPosition += _meterToSplineUnit * _speed * Time.deltaTime;
+        _parametricPosition %= 1.0f;
+
+        car.transform.position = spline.EvaluatePosition(_parametricPosition);
+        var splineTan = spline.EvaluateTangent(_parametricPosition);
+        var tan = new Vector3(splineTan.x, splineTan.y, splineTan.z);
+        car.transform.LookAt(car.transform.position + tan);
+    }
+}
