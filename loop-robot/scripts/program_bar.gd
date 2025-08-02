2c3fb14581dce47e23c1ee215cb4fbae9e4a44bb
Ricardo Bustamante <ricardo@busta.dev>
2025-08-02 04:41:54 -0300

    Make commands draggable and droppable in the command bar

--- loop-robot/scripts/program_bar.gd
+++ loop-robot/scripts/program_bar.gd
@@ -1,7 +1,12 @@
-extends Node
+extends HBoxContainer
+
+var buttons: Array
 
 func _get_drag_data(at_position: Vector2) -> Variant:
 	var data = {}
+	
+	var control = Control.new()
+	set_drag_preview(control)
 	
 	return data
 	
@@ -10,4 +15,15 @@ func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
 	return true
 	
 func _drop_data(at_position: Vector2, data: Variant) -> void:
-	pass
+	var drag_texture = TextureRect.new()
+	drag_texture.expand_mode = TextureRect.EXPAND_FIT_WIDTH
+	drag_texture.texture = data["texture"]
+	drag_texture.size = data["size"]
+	drag_texture.set_script("res://scripts/program_tile.gd")
+	
+	var control = Control.new()
+	self.add_child(control)
+	control.add_child(drag_texture)
+	drag_texture.get_rect().position = -drag_texture.size/2
+	set_drag_preview(control)
+	
--- loop-robot/scripts/program_button.gd
+++ loop-robot/scripts/program_button.gd
@@ -17,10 +17,3 @@ func _get_drag_data(at_position: Vector2) -> Variant:
 	set_drag_preview(control)
 	
 	return data
-	
-func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
-	print("Can drop data %s", at_position)
-	return true
-	
-func _drop_data(at_position: Vector2, data: Variant) -> void:
-	pass
